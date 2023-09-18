911e569b1ed86bf65235947c92acc8dc1a951346
Serveresty <cs.go.12228@gmail.com>
2023-09-18 03:39:42 +0300

    finish commit

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "GenerativeGrammar/models"
+import (
+	"GenerativeGrammar/internal/models"
+	"GenerativeGrammar/internal/services"
+	"fmt"
+)
 
 func main() {
 	rules := []models.Rules{{NotTerminal: "S", Terminal: "AA"}, {NotTerminal: "A", Terminal: "aAb"}, {NotTerminal: "A", Terminal: "ab"}}
+	fmt.Println("********* 1 вариант *********\n---------  Правила  ---------")
+	for _, el := range rules {
+		fmt.Print(el.NotTerminal + "->" + el.Terminal + "\n")
+	}
+	startWord := "S"
+	mapOfRules := services.GetCountOfTeminators(rules)
+	word := services.Generate(startWord, mapOfRules)
+	fmt.Println("Сгенерированное слово: " + word)
 }
---
+++ internal/services/service.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"GenerativeGrammar/internal/models"
+	"math/rand"
+	"strings"
+)
+
+func Generate(startWord string, rules map[string][]models.Rules) string {
+	if startWord != strings.ToLower(startWord) {
+		for startWord != strings.ToLower(startWord) {
+			for key, elem := range rules {
+				if len(elem) > 1 {
+					rndElem := rand.Intn(len(elem) - 0)
+					startWord = strings.Replace(startWord, key, elem[rndElem].Terminal, -1)
+				} else {
+					startWord = strings.Replace(startWord, key, elem[0].Terminal, -1)
+				}
+			}
+		}
+		return startWord
+	} else {
+		return startWord
+	}
+}
+
+func GetCountOfTeminators(rules []models.Rules) map[string][]models.Rules {
+	var ruleCount = make(map[string][]models.Rules, 0)
+	for _, elem := range rules {
+		ruleCount[elem.NotTerminal] = append(ruleCount[elem.NotTerminal], elem)
+	}
+	return ruleCount
+}
