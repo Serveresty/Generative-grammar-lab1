0ba66f0de0f762b899aefc2141916450eea357a5
Serveresty <cs.go.12228@gmail.com>
2023-10-07 00:28:13 +0300

    fix

--- Create-syntax-analyzer-lab6/internal/services/service.go
+++ Create-syntax-analyzer-lab6/internal/services/service.go
@@ -8,10 +8,15 @@ import (
 var arr = [][2]string{{"S", "var"}, {"S", "int"}, {"S", "float"}, {"S", "S+S"}, {"S", "S/S"}, {"S", "S*S"}, {"S", "S-S"}, {"S", "(S)"}}
 
 func CheckStr(str string) {
+	checker := ""
+	fmt.Println(str)
 	for str != "S" {
 		for _, elem := range arr {
 			str = strings.Replace(str, elem[1], elem[0], 1)
-			fmt.Println(str)
+			if checker != str {
+				fmt.Println(str)
+			}
+			checker = str
 		}
 	}
 }
